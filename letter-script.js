8dc3f8cc5ccf594086fd035a9cf1ec04d1777c20
Ammar Randera <ammarrandera@gmail.com>
2026-05-03 18:11:27 +0530

    Update letter-script.js

--- letter-script.js
+++ letter-script.js
@@ -24,7 +24,7 @@ const STATE = { ON: fromBirthday ? false : true };
 
 document.documentElement.style.setProperty("--on", STATE.ON ? "1" : "0");
 
-const TOGGLE_READY_AT = Date.now() + 7000;
+const TOGGLE_READY_AT = Date.now() + 2000;
 
 const CORD_DURATION = 0.1;
 const PULL_THRESHOLD = 15;
